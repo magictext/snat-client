841da8c8ed67628ca8fc8d74b4b488ca587728db
XILING <743145068@qq.com>
2020-03-10 16:28:40 +0800

    2020.3.10

--- src/main/java/config/ClientConfigFactory.java
+++ src/main/java/config/ClientConfigFactory.java
@@ -11,28 +11,7 @@ import java.util.Map;
 //客户端用来获得配置的类
 public class ClientConfigFactory {
 
-    private static DualHashBidiMap map=new DualHashBidiMap();
-
-        static{
-            ClientConfig config = Mapper.parseObject(new File("./target/classes/client.json"), ClientConfig.class);
-//            ourInstance=Mapper.parseObject(new File(System.getProperty("user.dir")+ LocalProxyRunner.configFileSrc), ClientConfig.class);
-            for (ConfigEntity entity : config.getList()) {
-                Map<Integer, Integer> port = null;
-                try {
-                    port = RangePort.getRangePort(entity.getLocalServer(),entity.getPort(), entity.getRemotePort());
-                } catch (RangePortException e) {
-                    e.printStackTrace();
-                }
-                map.putAll(port);
-            }
-            ourInstance=config;
-        }
-
-        private static ClientConfig ourInstance ;
-
-        public static ClientConfig getInstance() {
-            return ourInstance;
-        }
+        private static DualHashBidiMap map=new DualHashBidiMap();
 
         public static DualHashBidiMap getMap(){
             return map;
--- src/main/java/runner/LocalProxyRunner.java
+++ src/main/java/runner/LocalProxyRunner.java
@@ -6,10 +6,11 @@ import config.ClientConfig;
 import config.ClientConfigFactory;
 import exception.RangePortException;
 import io.netty.channel.Channel;
-import util.Data;
+import util.IpUtil;
 import util.Mapper;
 import util.Type;
 
+import java.io.File;
 import java.net.InetSocketAddress;
 import java.util.concurrent.BlockingDeque;
 
@@ -17,17 +18,18 @@ public class LocalProxyRunner {
 
 	public static Channel toServerChannel;
 
-	public  static String configFileSrc;
+	public  static ClientConfig config;
 
 	public static void main(String[] args) throws InterruptedException, RangePortException, JsonProcessingException {
-
 		if(args.length==0){
-
+			config = Mapper.parseObject(new File("./target/classes/client.json"), ClientConfig.class);
 		}else {
-			configFileSrc=args[0];
+			String configFileSrc=args[0];
+			config = Mapper.parseObject(new File(System.getProperty("user.dir") + configFileSrc), ClientConfig.class);
 		}
-		ClientConfig clientConfig = ClientConfigFactory.getInstance();
-		LocalProxyToServer localProxyToServerRunnable = new LocalProxyToServer(clientConfig);
+		config.setIpv4LocalAddress(IpUtil.getLocalIp(4));
+		config.setIpv6LocalAddress(IpUtil.getLocalIp(6));
+		LocalProxyToServer localProxyToServerRunnable = new LocalProxyToServer(config);
 		Thread toServerThread =new Thread(localProxyToServerRunnable,"localProxyToServerRunnable");
 		toServerThread.start();
 //		while (true){
